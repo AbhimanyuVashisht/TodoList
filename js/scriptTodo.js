f2adfaf472c8366dc132e7be8d6a92ef67ff5039
WillNotFit <abhimanyuvashisht.av@gmail.com>
2017-07-11 23:55:16 +0530

    Added move-up-down function
    Signed-off-by: WillNotFit <abhimanyuvashisht.av@gmail.com>

--- js/scriptTodo.js
+++ js/scriptTodo.js
@@ -23,6 +23,7 @@ $(function () {
     })
 });
 
+// Refresh the TodoList
 function refreshTodos(firstPageLoad = false) {
     if(!firstPageLoad){
         saveTodos();
@@ -38,26 +39,43 @@ function refreshTodos(firstPageLoad = false) {
     }
 }
 
+// Delete the particular Todo
 function deleteTodo(ev) {
     let todoId = $(ev.target).parent().attr('data-id');
     todos.splice(todoId,1);
     refreshTodos();
 }
 
+// Move TodoUp in the List
+function moveTodoUp(ev) {
+    let todoId = ($(ev.target).parent().attr('data-id'));
+    todos.splice((todoId-1),0,todos.splice(todoId,1)[0]);
+    refreshTodos();
+}
 
+// Move TodoDown in the List
+function moveTodoDown(ev) {
+    let todoId = ($(ev.target).parent().attr('data-id'));
+    todos.splice((todoId+1),0,todos.splice(todoId,1)[0]);
+    refreshTodos();
+}
 
+// Create the List Element with EventListeners
 function createTodoListItem(i) {
     let todoItem = $(`<li data-id="${i}" class="list-group-item"></li>`);
     todoItem.append($(`<input type="checkbox" class="col-1">`).attr('checked',todos[i].done));
     todoItem.append($(`<span class="col-8">${todos[i].task}</span>`));
     todoItem.append($(`<i class="fa fa-remove col-1 delete"></i>`)
         .click(deleteTodo));
-    todoItem.append($(`<i class="fa fa-chevron-up col-1 icn-move"></i>`));
-    todoItem.append($(`<i class="fa fa-chevron-down col-1 icn-move"></i>`));
+    todoItem.append($(`<i class="fa fa-chevron-up col-1 icn-move"></i>`)
+        .click(moveTodoUp));
+    todoItem.append($(`<i class="fa fa-chevron-down col-1 icn-move"></i>`)
+        .click(moveTodoDown));
 
     return todoItem;
 }
 
+// Retrieve Todos from the LocalStorage
 function retrieveTodos() {
     let savedTodos = localStorage.getItem('todos');
     if(savedTodos){
@@ -65,11 +83,13 @@ function retrieveTodos() {
     }
 }
 
-
+// Save Todos to the Local Storage
 function saveTodos() {
     // Storing the list everyTime with any update in it
     localStorage.setItem("todos", JSON.stringify(todos));
 }
+
+// Add Todos to the List
 function addTodo(todoTask) {
     todos.push(new Todo(todoTask, false));
     refreshTodos();
